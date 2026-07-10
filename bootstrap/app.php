b10cb95f27b95d37b4d33adf025082f97e956f00
gennosukenokoga <jsofthouse@gmail.com>
2026-07-10 16:07:23 +0700

    Test: coba copot logvisitor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            \App\Http\Middleware\LogVisitor::class,
-        ]);
+        // $middleware->web(append: [
+        //     \App\Http\Middleware\LogVisitor::class,
+        // ]);
 
         $middleware->trustProxies(at: [
             '173.245.48.0/20',
