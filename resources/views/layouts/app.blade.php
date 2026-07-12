700c199a569a5c478c0e62b53505d084b28d49d1
gennosukenokoga <jsofthouse@gmail.com>
2026-07-12 20:55:46 +0700

    feat: tambah meta head csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Title & Description (SEO) -->
     <title>NgajiNusa - Kursus Ngaji Online Bersama Guru Bersertifikat</title>
