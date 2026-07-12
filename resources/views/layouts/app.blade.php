df3f853c135f31cc944acea6a0443cf6e43cb724
gennosukenokoga <jsofthouse@gmail.com>
2026-07-12 10:42:25 +0700

    feat: ubah css/app.css menggunakan filemtime

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}" />
     @stack('styles')
 </head>
 
